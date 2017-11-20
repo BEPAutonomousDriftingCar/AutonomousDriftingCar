b8fb35dc8ac1a0f1ca0c6a5166cb9fe874817a02
Unknown <st.maartenk@gmail.com>
2017-11-20 16:30:05 +0100

    First sample of 4 working pulse counters working at the same time

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -8,15 +8,23 @@ RevCounter RevCounter;
 
 //(1) Setup statements
 void setup() {
-	RevCounter.begin();
-
-
-//Set up serial connection to computer
+  //Set up serial connection to computer
  Serial.begin(9600);
- Serial.println(FTM1Ch0CountValue);
+ delay(500);
  Serial.println("All set up, lets begin");
- pinMode(13, OUTPUT);
- digitalWrite(13, HIGH);
+
+
+ 
+	RevCounter.begin();
+ Serial.println(TPM1_SC);
+
+pinMode(13, OUTPUT);
+digitalWrite(13, HIGH);
+
+
+
+
+
 }
 
 //(3) main loop statements
@@ -24,7 +32,13 @@ void loop() {
 //read FTM1 Ch0 value if valid
 FTM1Ch0CountValue = FTM1_CNT; //read CH0 value
 //rpm =analogRead(9)*3300/1024;
-Serial.println(FTM1Ch0CountValue);
+Serial.print(FTM1_CNT);
+Serial.print(" ");
+Serial.print(FTM2_CNT);
+Serial.print(" ");
+Serial.print(TPM1_CNT);
+Serial.print(" ");
+Serial.println(TPM2_CNT);
 //Serial.print(" ");
 //Serial.println(rpm);
 delay(499);
--- Arduino/RevCounter.cpp
+++ Arduino/RevCounter.cpp
@@ -26,6 +26,11 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 
 RevCounter::RevCounter(){}
 void RevCounter::begin() {
+
+//Set up System options
+SIM_SOPT4 |= 0x04000000; //uses FTM_CLKIN0 and FTM_CLKIN1 for external clock signals
+SIM_SCGC2 |= 0x600; // Set Gate clock select to enable TPM2 and TPM1
+
 	
 //configure Flextimer 1
 FTM1_MODE = 0x05; //set write-protect disable (WPDIS) bit to modify other registers
@@ -43,17 +48,40 @@ FTM2_CNTIN = 0;
 FTM2_CNT = 0x0000; //reset count to zero
 FTM2_MOD = 0xFFFF; //max modulus = 9999 (gives count = 10000 on roll-over) 
 
+//configure TPM1
+TPM1_SC &= ~0x18; //set status/control to zero = disable all internal clock sources
+TPM1_CNT = 0x0000; //reset count to zero
+TPM1_MOD = 0xFFFF; //max modulus = 9999 (gives count = 10000 on roll-over)
+TPM1_C0SC=0x00;
+TPM1_C1SC=0x00; 
+TPM1_QDCTRL = 0x0F; //see section 36.3.21 of ref manual for details 
+
+//configure TPM2
+TPM2_SC &= ~0x18; //set status/control to zero = disable all internal clock sources
+TPM2_CNT = 0x0000; //reset count to zero
+TPM2_MOD = 0xFFFF; //max modulus = 9999 (gives count = 10000 on roll-over)
+TPM2_C0SC=0x00;
+TPM2_C1SC=0x00; 
+TPM2_QDCTRL = 0x0F; //see section 36.3.21 of ref manual for details 
+
+
 
 //configure Teensy port pins
 PORTB_PCR16 |= 0x400;  //MUX = FTM_CLKIN0 ALT4 on Chip Pin 95 = Teensy Pin 0
 PORTB_PCR17 |= 0x400;  //MUX = FTM_CLKIN1 ALT4 on Chip Pin 95 = Teensy Pin 1
+PORTA_PCR12 |= 0x700; //MUX = alternative function 7 on Chip Pin 28 (TPM1_QD_PHA) = Teensy Pin 3 
+PORTA_PCR13 |= 0x700; //MUX = alternative function 7 on Chip Pin 29 (TPM1_QD_PHB) = Teensy Pin 4 
+PORTB_PCR18 |= 0x600; //MUX = alternative function 6 on Chip Pin 97 (TPM2_QD_PHA) = Teensy Pin 29 
+PORTB_PCR19 |= 0x600; //MUX = alternative function 6 on Chip Pin 98 (TPM2_QD_PHB) = Teensy Pin 30
 
 //initialise SIM_SOPT4
-SIM_SOPT4 = 0x04000000; //uses FTM_CLKIN0 and FTM_CLKIN1 for external clock signals
 
-//set counter to use external clocks
+
+//set counter to use external clocks and enable counters
 FTM1_SC = 0x18; // (Note – FTM1_SC [TOF=0 TOIE=0 CPWMS=0 CLKS=11 (external clocks selected) PS=000 [no prescale divide])
 FTM2_SC = 0x18; // (Note – FTM2_SC [TOF=0 TOIE=0 CPWMS=0 CLKS=11 (external clocks selected) PS=000 [no prescale divide])
+TPM1_SC = 0x08;
+TPM2_SC = 0x08;
 }
 
 

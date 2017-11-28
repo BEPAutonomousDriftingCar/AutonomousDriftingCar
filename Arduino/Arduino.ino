b7aad01b5af2f7eca38313290f036455bd34b8de
Unknown <st.maartenk@gmail.com>
2017-11-28 15:11:57 +0100

    Ros control for ESC and servo
    Build in RC servo and Esc commands+added ros support for controlling the vehicle

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -7,6 +7,24 @@
 #include <sensor_msgs/MagneticField.h>
 #include "MPU9250.h"
 
+#include <Servo.h> 
+#include <std_msgs/UInt16.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/Empty.h>
+#include <geometry_msgs/Twist.h>
+
+// These are general bounds for the steering servo and the
+// TRAXXAS Electronic Speed Controller (ESC)
+const int minSteering = 55 ;
+const int maxSteering = 135 ;
+const int minThrottle = 1000 ;
+const int maxThrottle = 2000 ;
+
+Servo steeringServo;
+Servo electronicSpeedController ;  // The ESC on the TRAXXAS works like a Servo
+
+
 RevCounter RevCounter;
 const float pi = 3.14159265359;
 
@@ -18,11 +36,50 @@ tf::TransformBroadcaster broadcaster;
 char base_link[] = "/base_link";
 char odom[] = "/odom";
 char frame_id[] = "imu";
+double lastTime, nowTime;
+double samplePeriod = 0.010;
 sensor_msgs::Imu imu_msg;
-sensor_msgs::MagneticField magneticfield_msg;
+sensor_msgs::MagneticField mag_msg;
 ros::Publisher pub("imu/data_raw", &imu_msg);
-ros::Publisher pub2("imu/mag", &magneticfield_msg);
+ros::Publisher magpub("imu/mag", &mag_msg);
 
+void driveCallback ( const geometry_msgs::Twist&  twistMsg )
+{
+  
+  int steeringAngle = fmap(twistMsg.angular.z, -1.0, 1.0, minSteering, maxSteering) ;
+  // The following could be useful for debugging
+  // str_msg.data= steeringAngle ;
+  // chatter.publish(&str_msg);
+  // Check to make sure steeringAngle is within car range
+  if (steeringAngle < minSteering) { 
+    steeringAngle = minSteering;
+  }
+  if (steeringAngle > maxSteering) {
+    steeringAngle = maxSteering ;
+  }
+  steeringServo.write(steeringAngle) ;
+  
+  // ESC forward is between 0.5 and 1.0
+  int escCommand ;
+  //if (twistMsg.linear.x >= 0.5) {
+  escCommand = (int)fmap(twistMsg.linear.x, -20.0, 20.0, minThrottle, maxThrottle) ;
+  //} else {
+  //  escCommand = (int)fmap(twistMsg.linear.x, 0.0, 1.0, 0.0, 180.0) ;
+  //}
+  // Check to make sure throttle command is within bounds
+  if (escCommand < minThrottle) { 
+    escCommand = minThrottle;
+  }
+  if (escCommand > maxThrottle) {
+    escCommand = maxThrottle ;
+  }
+  // The following could be useful for debugging
+  // str_msg.data= escCommand ;
+  // chatter.publish(&str_msg);
+  electronicSpeedController.writeMicroseconds(escCommand);
+} 
+
+ros::Subscriber<geometry_msgs::Twist> driveSubscriber("/cmd_vel", &driveCallback) ;
 
 // an MPU9250 object with the MPU-9250 sensor on Teensy Chip Select pin 10
 MPU9250 IMU(10);
@@ -31,10 +88,9 @@ int beginStatus;
 
 
 void setup(){
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);  
   nh.initNode();
   nh.advertise(pub);
+  nh.advertise(magpub);
   broadcaster.init(nh);
   
   RevCounter.begin();
@@ -44,6 +100,22 @@ void setup(){
   // ACCELEROMETER 2G 4G 8G 16G
   // GYRO 250DPS 500DPS 1000DPS 2000DPS
   beginStatus = IMU.begin(ACCEL_RANGE_4G,GYRO_RANGE_250DPS);
+
+  mag_msg.header.seq = 0;
+  mag_msg.header.stamp = nh.now();
+  mag_msg.header.frame_id = frame_id;
+
+  mag_msg.magnetic_field_covariance[0] = 0;
+  mag_msg.magnetic_field_covariance[1] = 0;
+  mag_msg.magnetic_field_covariance[2] = 0;
+
+  mag_msg.magnetic_field_covariance[3] = 0;
+  mag_msg.magnetic_field_covariance[4] = 0;
+  mag_msg.magnetic_field_covariance[5] = 0;
+
+  mag_msg.magnetic_field_covariance[6] = 0;
+  mag_msg.magnetic_field_covariance[7] = 0;
+  mag_msg.magnetic_field_covariance[8] = 0;
 
   imu_msg.header.seq = 0;
   imu_msg.header.stamp = nh.now();
@@ -84,45 +156,72 @@ void setup(){
   imu_msg.linear_acceleration_covariance[6] = 0;
   imu_msg.linear_acceleration_covariance[7] = 0;
   imu_msg.linear_acceleration_covariance[8] = 0;
+
+  nh.subscribe(driveSubscriber) ;
+  // Attach the servos to actual pins
+  steeringServo.attach(9); // Steering servo is attached to pin 9
+  electronicSpeedController.attach(8); // ESC is on pin 10
+  // Initialize Steering and ESC setting
+  // Steering centered is 90, throttle at neutral is 90
+  steeringServo.write(90) ;
+  electronicSpeedController.writeMicroseconds(1500) ;
+  delay(1000);
+  lastTime = nh.now().toSec();
 }
 
 void loop(){
-  IMU.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &hx, &hy, &hz);
+  nowTime=nh.now().toSec();
+  if(nowTime-lastTime >= samplePeriod){
+    IMU.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &hx, &hy, &hz);
   
-  imu_msg.header.seq++;
-  imu_msg.header.stamp = nh.now();
+    imu_msg.header.seq++;
+    imu_msg.header.stamp = nh.now();
 
-  imu_msg.orientation.x = 0;
-  imu_msg.orientation.y = 0;
-  imu_msg.orientation.z = 0;
-  imu_msg.orientation.w = 0;
+    imu_msg.orientation.x = 0;
+    imu_msg.orientation.y = 0;
+    imu_msg.orientation.z = 0;
+    imu_msg.orientation.w = 0;
 
-  imu_msg.linear_acceleration.x = ax;
-  imu_msg.linear_acceleration.y = ay;
-  imu_msg.linear_acceleration.z = az;
+    imu_msg.linear_acceleration.x = ax;
+    imu_msg.linear_acceleration.y = ay;
+    imu_msg.linear_acceleration.z = az;
 
-  imu_msg.angular_velocity.x = gx;
-  imu_msg.angular_velocity.y = gy;
-  imu_msg.angular_velocity.z = gz;
+    imu_msg.angular_velocity.x = gx;
+    imu_msg.angular_velocity.y = gy;
+    imu_msg.angular_velocity.z = gz;
 
-  magneticfield_msg.magnetic_field.x = hx;
-  magneticfield_msg.magnetic_field.y = hy;
-  magneticfield_msg.magnetic_field.z = hz;
+    mag_msg.header.seq++;
+    mag_msg.header.stamp = nh.now();
+    
+    mag_msg.magnetic_field.x = hx;
+    mag_msg.magnetic_field.y = hy;
+    mag_msg.magnetic_field.z = hz;
 
-  t.header.frame_id = odom;
-  t.child_frame_id = base_link;
+    t.header.frame_id = odom;
+    t.child_frame_id = base_link;
 
-  t.transform.rotation.x = RevCounter.readCounter(0)*pi;
-  t.transform.rotation.y = RevCounter.readCounter(1)*pi; 
-  t.transform.rotation.z = RevCounter.readCounter(2)*pi; 
-  t.transform.rotation.w = RevCounter.readCounter(3)*pi;  
+    t.transform.rotation.x = RevCounter.readCounter(0)*pi;
+    t.transform.rotation.y = RevCounter.readCounter(1)*pi; 
+    t.transform.rotation.z = RevCounter.readCounter(2)*pi; 
+    t.transform.rotation.w = RevCounter.readCounter(3)*pi;  
+    
+    t.header.stamp = nh.now();
   
-  t.header.stamp = nh.now();
-  
-  pub.publish(&imu_msg);
-  pub2.publish(&magneticfield_msg);
-  broadcaster.sendTransform(t);
+    pub.publish(&imu_msg);
+    magpub.publish(&mag_msg);
+    broadcaster.sendTransform(t);
+    lastTime=nh.now().toSec();
+  }
+  if(nh.connected() == false){
+    electronicSpeedController.write(1490);
+  }
   nh.spinOnce();
-
-  delay(10);
+  delay(1);
 }
+
+// Arduino 'map' funtion for floating point
+double fmap (double toMap, double in_min, double in_max, double out_min, double out_max) {
+  return (toMap - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
